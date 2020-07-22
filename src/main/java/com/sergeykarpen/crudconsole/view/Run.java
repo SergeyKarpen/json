bb988dc26e4661e8dd704b42914d352631df961a
Sergey Karpenchyk <karp.en@inbox.ru>
2020-07-22 23:49:43 +0300

    add view

--- src/main/java/com/sergeykarpen/crudconsole/view/Run.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.sergeykarpen.crudconsole.view;
-
-public class Run {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/com/sergeykarpen/crudconsole/view/View.java
@@ -0,0 +1,15 @@
+package main.java.com.sergeykarpen.crudconsole.view;
+
+public class View {
+    private final String menuSelectionMessage = "Choose an action:\n" +
+            "1.View the list of skills\n" +
+            "2.Select skill by id\n" +
+            "3.Add new skill\n" +
+            "4.Delete skill\n" +
+            "5.Modify an existing skill\n" +
+            "6.Exit\n";
+
+
+
+}
+
